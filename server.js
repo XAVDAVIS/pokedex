67af33e03e4dd2c135107cced2727e0d4fc9fbf7
Xavier <shottyx5156@hotmail.com>
2022-12-14 23:57:22 -0500

    adds create post

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-/////// Variables ///////
+                    /////// Variables ///////
 const express = require("express")
 const app = express();
 const port = 3000; 
-const Pokemon = require('./models/pokemon.js');
+const Pokemon = require('./pokedex/models/pokemon.js');
 
 ////// Listen //////
 app.listen(port, () => {
     console.log(`listening to pokedex on port ${port}`)
 });
 
+                      ////// GETS ///////
+app.get('/pokedex', (req, res ) => {
+    res.render('pokedex.ejs')
+})
 
 ////// Index //////
 app.get('/', (req, res ) => {
@@ -20,5 +24,30 @@ app.get('/:id', (req, res ) => {
     res.render('show.ejs', { data: Pokemon[req.params.id] });
 });
 
+/////// NEW ///////
+app.get('/pokedex/new', (req, res ) => {
+    res.render('new.ejs');
+})
 
+/////// EDIT ///////
+app.get('/pokedex/:index/edit', (req, res ) => {
+    res.render('edit.ejs', {
+        pokemon: Pokemon[req.params.index],
+        index: req.params.indexOfPokemonArray
+    })
+})
 
+/////// CREATE ///////
+app.post('/pokedex', (req, res ) => {
+    const myStats = {
+        hp: req.body.hp,
+        attack: req.body.attack,
+        defense: req.body.defense, 
+        spattack: req.body.spattack, 
+        spdefense: req.body.spdefense,
+        speed: req.body.speed
+    };
+        req.body.stats = myStats;
+        Pokemon.push(req.body);
+        res.redirect('/pokedex');
+});
