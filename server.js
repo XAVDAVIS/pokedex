7deb3096c0bd99460921b6f3c335f6d15abec790
Xavier <shottyx5156@hotmail.com>
2022-12-14 20:12:01 -0500

    Added index body

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 /////// Variables ///////
 const express = require("express")
+const app = express();
+const port = 3000; 
+const Pokemon = require('./models/pokemon.js');
+
+////// Listen //////
+app.listen(port, () => {
+    console.log(`listening to pokedex on port ${port}`)
+});
+
+
+////// Index //////
+app.get('/', (req, res ) => {
+    res.render('index.ejs', {data: Pokemon });
+});
+
+////// Show //////
+app.get('/:id', (req, res ) => {
+    res.render('show.ejs', { data: Pokemon[req.params.id] });
+});
+
+
+
