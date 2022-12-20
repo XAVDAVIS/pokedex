01889b4017c5cb3cd2de59371217eba190c151e2
Xavier <shottyx5156@hotmail.com>
2022-12-20 09:09:31 -0500

    updated index, show and server.js

--- server.js
+++ server.js
@@ -1,44 +1,66 @@
                     /////// Variables ///////
-const express = require("express")
+const express = require("express");
+const methodOverride = require("method-override");
+const Pokemon = require("./models/pokemon.js");
 const app = express();
-const port = 3000; 
-const Pokemon = require('./pokedex/models/pokemon.js');
+const port = 3000;
 
-////// Listen //////
-app.listen(port, () => {
-    console.log(`listening to pokedex on port ${port}`)
+                    /////// MIDDLEWARE ////////
+app.use(methodOverride("_method") );
+app.use(express.urlencoded( {extended: false}));
+
+// app.use(( req, res, next) => {
+//     console.log('I am running for all routes')
+//     next()
+// })
+
+// SEED ROUTE //
+app.get('/pokedex/seed', (req, res) => {
+    Pokemon.deleteMany({}, (err) => {
+        Pokemon.create(data, (err) => {
+            res.redirect('/pokedex');
+        });
+    });
 });
 
                       ////// GETS ///////
-app.get('/pokedex', (req, res ) => {
-    res.render('pokedex.ejs')
-})
+// app.get('/pokedex', (req, res ) => {
+//     res.render("pokedex.ejs")
+// })
 
 ////// Index //////
-app.get('/', (req, res ) => {
-    res.render('index.ejs', {data: Pokemon });
-});
-
-////// Show //////
-app.get('/:id', (req, res ) => {
-    res.render('show.ejs', { data: Pokemon[req.params.id] });
+app.get('/pokedex', (req, res ) => {
+    res.render("index.ejs", {
+    myPokemon: Pokemon,
+    });
 });
 
 /////// NEW ///////
-app.get('/pokedex/new', (req, res ) => {
+app.get("/pokedex/new", (req, res ) => {
     res.render('new.ejs');
 })
-
-/////// EDIT ///////
-app.get('/pokedex/:index/edit', (req, res ) => {
-    res.render('edit.ejs', {
-        pokemon: Pokemon[req.params.index],
-        index: req.params.indexOfPokemonArray,
-    })
+/////// DELETE ///////
+app.delete("/pokedex/:index", (req, res ) => {
+    Pokemon.splice(req.params.indexOfPokemonArray, 1)
+    res.redirect("/pokedex");
+});
+/////// UPDATE ///////
+app.put("/pokedex/:index", (req, res ) => {
+    const myStats = {
+        hp: req.body.hp,
+        attack: req.body.attack,
+        defense: req.body.defense, 
+        spattack: req.body.spattack, 
+        spdefense: req.body.spdefense,
+        speed: req.body.speed
+    };
+    req.body.stats = myStats;
+    Pokemon[req.params.indexOfPokemonArray] = req.body;
+    res.redirect("/pokedex");
 })
 
 /////// CREATE ///////
-app.post('/pokedex', (req, res ) => {
+app.post("/pokedex", (req, res ) => {
     const myStats = {
         hp: req.body.hp,
         attack: req.body.attack,
@@ -49,26 +71,27 @@ app.post('/pokedex', (req, res ) => {
     };
         req.body.stats = myStats;
         Pokemon.push(req.body);
-        res.redirect('/pokedex');
+        res.redirect("/pokedex");
 });
 
-/////// UPDATE ///////
-app.put('/pokedex/:indexOfPokemonArray', (req, res ) => {
-    const myStats = {
-        hp: req.body.hp,
-        attack: req.body.attack,
-        defense: req.body.defense, 
-        spattack: req.body.spattack, 
-        spdefense: req.body.spdefense,
-        speed: req.body.speed
-    };
-    req.body.stats = myStats;
-    myPokemon[req.params.indexOfPokemonArray] = req.body;
-    res.redirect('/pokedex');
+
+
+/////// EDIT ///////
+app.get("/pokedex/:index/edit", (req, res ) => {
+    res.render("edit.ejs", {
+        myPokemon: Pokemon[req.params.index],
+        index: req.params.indexOfPokemonArray,
+    })
 })
 
-/////// DESTROY ///////
-app.delete('/pokedex/:index', (req, res ) => {
-    Pokemon.splice(req.params.indexOfPokemonArray, 1)
-    res.redirect('/pokedex');
+////// Show //////
+app.get('/:id', (req, res ) => {
+    res.render("show.ejs", { data: Pokemon[req.params.id] });
+});
+
+
+// Listen 
+
+app.listen(port, () => {
+    console.log(`listening to pokedex on port ${port}`)
 });
